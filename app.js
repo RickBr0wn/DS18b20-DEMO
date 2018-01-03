3685fa8ae2371735a786d1bd3e7f1b9b0a65bb6a
RiCK BROWN <ricky.brown.00@gmail.com>
2018-01-03 10:46:23 +0000

    First commit

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ app.use(express.static(__dirname + '/public'));
 
 // board.on
 board.on("ready", function() {
-  // Connection meassage in the console
+  // Connection message in the console
   console.log('ARDUINO BOARD READY STATE: TRUE');
 
+  // As an example, i have included here the code which enables a DS18B20 Temperature Sensor 
   // This requires OneWire support using the ConfigurableFirmata
   // Create a thermometer instance
   let thermometer = new five.Thermometer({
@@ -30,26 +31,28 @@ board.on("ready", function() {
 
   // On.change
   thermometer.on("change", function() {
-    // Prints data to 'server' console
+    //Prints data to 'server' console
     console.log(this.celsius + "°C");
-    // console.log("0x" + this.address.toString(16));
+    //console.log("0x" + this.address.toString(16));
 
-    // Send data via io
-    io.emit('data', this.celsius);
-  });
+  // Send data via sockets.io
+  io.emit('data', this.celsius);
+  }); // End of thermometer
 });
 
-// Begin 'listening' on pre defined port number
+// Begin 'listening' on the pre defined port number (3000)
 const server = http.createServer(app).listen(port, function(req, res){
   console.log('LISTENING ON PORT ' + port);
 });
 
-// Set up socket.io
+// Set up socket.io to 'listen'
 io = io.listen(server);
 
+// Display a conection message
 io.on('connection', function(socket){
   console.log('SOCKET.IO CONNECTED');
 
+  // Display a disconnection message
   socket.on('disconnect', function(){
     console.log('SOCKET.IO DISCONNECTED');
   })
